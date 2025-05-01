a788d1f91c1207af09f16954d1a794534db4db6a
PashaShanon <pashaakhsyuhada9@gmail.com>
2025-05-01 13:07:49 +0700

    cleaning

--- api.py
+++ api.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
+from fastapi  import FastAPI
 from pydantic import BaseModel
 import model_predict
 
 app = FastAPI()
-
-class Content(BaseModel):
-    text: str
+class Content(BaseModel): text: str
 
 @app.post("/api/predict")
 def predict(data: Content):
 
-    print(model_predict.predict(data.text, "XGB"))
-    return {"text":data.text, "rating": model_predict.predict(data.text, "XGB")}
+    prediction = model_predict.predict(data.text, "XGB")
+    print(f"""
+data  :   {data!r}
+result:   {prediction!r}""")
+
+    return{"text": data.text, "rating": prediction}
