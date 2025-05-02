095e95ab1ebe4ec7696ffca84b32a882a14ddc8b
PashaShanon <pashaakhsyuhada9@gmail.com>
2025-05-02 15:59:56 +0700

    add array of texts support

--- api.py
+++ api.py
@@ -4,6 +4,8 @@ import model_predict
 
 app = FastAPI()
 class Content(BaseModel): text: str
+class ContentMass(BaseModel): texts: list[str]
+
 
 @app.post("/api/predict")
 def predict(data: Content):
@@ -13,4 +15,17 @@ def predict(data: Content):
 data  :   {data!r}
 result:   {prediction!r}""")
 
-    return{"text": data.text, "is_positive": prediction}
+    return {"text": data.text, "is_positive": prediction}
+
+@app.post("/api/predict-mass")
+def predict_mass(data: ContentMass):
+    
+    predictions = []
+    for line in data.texts:
+        prediction = model_predict.predict(line, "XGB")
+        predictions.append(prediction)
+        print(f"""
+data  :   {line!r}
+result:   {prediction!r}""")
+        
+    return {"texts": data.texts, "is_positives": predictions}
--- call.ps1
+++ call.ps1
@@ -10,8 +10,20 @@ function Send-API {
         -d "$json"
 }
 
-Send-API predict '
-    {
-        "text": "produk berkualitas, boleh beli lagi"
-    }
-'
+function Send-Texts {
+    param (
+        [string[]]$texts
+    )
+
+    $json = @{ texts = $texts } | ConvertTo-Json -Depth 2
+
+    # Use curl.exe if on Windows and you want native curl, not Invoke-WebRequest
+    curl.exe -X POST "http://127.0.0.1:8000/api/predict-mass" `
+        -H "Content-Type: application/json" `
+        -d $json
+
+    # Write-Output $json
+}
+
+
+Send-Texts "Barang yg kualitasnya bagus banget", "knp yg datang pecah semua???", "gimana sih ni yg jual, barang kok gk lengkap???", "makasih banget bang, barang dah dateng tepat waktu!", "10 jam barang baru sampai... pdhl hrusnya 5 jam", "mas kurirnya ternyata dtng lebih cepet"
