d422d2273150c58ebd4155e48bbeeed74d9f4069
PashaShanon <pashaakhsyuhada9@gmail.com>
2025-05-05 11:50:27 +0700

    depricate single string API call

--- api.py
+++ api.py
@@ -3,19 +3,7 @@ from pydantic import BaseModel
 import model_predict
 
 app = FastAPI()
-class Content(BaseModel): text: str
 class ContentMass(BaseModel): texts: list[str]
-
-
-@app.post("/api/predict")
-def predict(data: Content):
-
-    prediction = model_predict.predict(data.text, "XGB")
-    print(f"""
-data  :   {data!r}
-result:   {prediction!r}""")
-
-    return {"text": data.text, "is_positive": prediction}
 
 @app.post("/api/predict-mass")
 def predict_mass(data: ContentMass):
--- call.ps1
+++ call.ps1
@@ -4,7 +4,6 @@ function Send-API {
         [string]$json
     )
 
-    # Use curl.exe if on Windows and you want native curl, not Invoke-WebRequest
     curl.exe -X POST "http://127.0.0.1:8000/api/$cmd" `
         -H "Content-Type: application/json" `
         -d "$json"
@@ -17,13 +16,17 @@ function Send-Texts {
 
     $json = @{ texts = $texts } | ConvertTo-Json -Depth 2
 
-    # Use curl.exe if on Windows and you want native curl, not Invoke-WebRequest
     curl.exe -X POST "http://127.0.0.1:8000/api/predict-mass" `
         -H "Content-Type: application/json" `
         -d $json
 
-    # Write-Output $json
 }
 
 
-Send-Texts "Barang yg kualitasnya bagus banget", "knp yg datang pecah semua???", "gimana sih ni yg jual, barang kok gk lengkap???", "makasih banget bang, barang dah dateng tepat waktu!", "10 jam barang baru sampai... pdhl hrusnya 5 jam", "mas kurirnya ternyata dtng lebih cepet"
+Send-Texts `
+"Barang yg kualitasnya bagus banget", 
+"knp yg datang pecah semua???", 
+"gimana sih ni yg jual, barang kok gk lengkap???", 
+"makasih banget bang, barang dah dateng tepat waktu!", 
+"10 jam barang baru sampai... pdhl hrusnya 5 jam", 
+"mas kurirnya ternyata dtng lebih cepet"
